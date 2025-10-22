b241a7d9ebb4beecc6096fa6294741a46604479e
Felipe Possari <fcpfelipe@hotmail.com>
2025-10-22 11:15:26 -0300

    Hello word with gpt

---
+++ 1-hello-world.py
@@ -0,0 +1,7 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+model = ChatOpenAI(model_name="gpt-5-nano", temperature=0.5)
+message = model.invoke("Hello world!")
+print(message)
